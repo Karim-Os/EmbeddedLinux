f00246929660a29153ae772c2c9e099a26cda7cc
Karim-Os <KarimOsama1610@gmail.com>
2024-06-13 16:00:17 +0300

    Adding Second Python Task

--- PythonTasks/Task_2.py
+++ PythonTasks/Task_2.py
@@ -1,4 +1,4 @@
-#Task 1 : Write a Python program which accepts the radius of a circle from the user and compute the area
+#Task 2 : Write a Python program which accepts the radius of a circle from the user and compute the area
 import math
 radius = float(input ("Please input the radius in m to calculate the area of the circle \n"))
 area = round (radius**2 *math.pi,2)
---
+++ PythonTasks/Task_3.py
@@ -0,0 +1,6 @@
+#Task 3 Print the calendar of a given month and year:
+import calendar
+year = int (input ("Please inter the year value \n"))
+month = int (input ("Please inter the Month value \n"))
+
+print (calendar.month(year,month))
